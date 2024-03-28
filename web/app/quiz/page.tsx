6b35a5b77590db53d34283409293e5a3c398f212
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-03-28 10:24:02 +0100

    Update The Quiz Component, Add Link To Back Icon

--- web/app/quiz/page.tsx
+++ web/app/quiz/page.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
+import Dashboard from '../dashboard/page'
 
 const Quiz = () => {
   return (
     <div>
         <h2 className='text-cyan-800 text-end text-3xl m-10 font-bold'>QUIMER</h2>
         <div className='flex text-cyan-800 text-xl justify-between m-12'>
-        <svg className="h-8 w-8 text-cyan-800 hover:text-cyan-300"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="5" y1="12" x2="19" y2="12" />  <line x1="5" y1="12" x2="11" y2="18" />  <line x1="5" y1="12" x2="11" y2="6" />
-        </svg>
+        <Link href="/dashboard">
+            <svg className="h-8 w-8 text-cyan-800 hover:text-cyan-300"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="5" y1="12" x2="19" y2="12" />  <line x1="5" y1="12" x2="11" y2="18" />  <line x1="5" y1="12" x2="11" y2="6" />
+            </svg>
+        </Link>
         <h2 className='text-2xl'>02 of 60</h2>
         <h2 className='text-2xl'>Time: 10:13</h2>
         </div>

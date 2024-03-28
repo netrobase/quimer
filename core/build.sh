46927b1d3eacb68de9817cbda276dc36dc381b1b
Leonard Nzekwe <nzekweleo@gmail.com>
2024-03-28 10:30:32 +0100

    Fix Vercel Deployment: Pip & Modules Not Found

--- core/build.sh
+++ core/build.sh
@@ -2,7 +2,11 @@
 # API Django Build script
 
 echo "----Starting Build-----"
-echo "..Install requirements.."
+echo "-----Install PIP-------"
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+python3.9 -m pip --version
+echo "--Install Dependencies--"
 python3.9 -m pip install -r requirements.txt
 echo "------CHECK CONFIG-----"
 python3.9 manage.py check
